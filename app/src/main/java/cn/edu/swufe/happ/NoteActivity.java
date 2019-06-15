c2d9ea7eeb101d3ba1f2a87c7114663611de5d86
HP <734585607>
2019-06-15 17:03:28 +0800

    闪退闪退！

--- app/src/main/java/cn/edu/swufe/happ/NoteActivity.java
+++ app/src/main/java/cn/edu/swufe/happ/NoteActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class NoteActivity extends AppCompatActivity {
      public final String TAG = "NoteActivity";
-
+     TextView note1;
+     ListView note2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,11 @@ public class NoteActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String add_note = intent.getStringExtra("putin_note");
 
+        note1 = (TextView)findViewById(R.id.list_text);
+        //note2 = (ListView) findViewById(R.id.list_view);
+
+        note1.setText(add_note);
+       // note2.put(add_note);
 
         Log.i(TAG,"onCreate:add_note+"+ add_note);
 
